5e350d3cec21c3a52ef677b25f13462fd05ca917
zhangshaojie <15210062041@163.com>
2023-05-05 18:09:26 +0800

    zsj-master-commit 1

--- src/main/java/com/atguigu/GigMain.java
+++ src/main/java/com/atguigu/GigMain.java
@@ -4,6 +4,11 @@ public class GigMain {
     public static void main(String[] args) {
         System.out.println("22222222");
         System.out.println("4444444");
+        System.out.println("master");
         System.out.println("hot-fix");
+    }
+    public void testma(){
+        System.out.println("master");
+
     }
 }
--- src/main/java/com/atguigu/Tets.java
+++ src/main/java/com/atguigu/Tets.java
@@ -4,5 +4,6 @@ public class Tets {
 
 public void test(){
     System.out.println("hot-fix");
+    System.out.println("master");
 }
 }
